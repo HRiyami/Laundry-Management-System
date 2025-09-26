c1971b2ea6b21b7e9bf3f466aa82b7c8a99bcdd2
Haroon <oman.star91@gmail.com>
2025-09-26 08:37:19 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,23 @@
-// firebase-config.js
-// This file initializes Firebase and exports the Firestore and Functions instances.
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
-import { getFunctions } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-functions.js';
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDyMqHIQLV8h4KCAM0wCnyVSO6oMm5pOyw",
+    authDomain: "laundry-management-syste-b9141.firebaseapp.com",
+    projectId: "laundry-management-syste-b9141",
+    storageBucket: "laundry-management-syste-b9141.firebasestorage.app",
+    messagingSenderId: "995634723047",
+    appId: "1:995634723047:web:d0696025acdf6857080a42",
+    measurementId: "G-749F0ECMJG"
+  };
 
-// TODO: Replace the following configuration with your Firebase project's details.
-// You can find these settings in your Firebase console > Project settings > General.
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const functions = getFunctions(app);
-
-// Optionally set region for functions if different from default
-// import { connectFunctionsEmulator } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-functions.js';
-// connectFunctionsEmulator(functions, 'localhost', 5001);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
